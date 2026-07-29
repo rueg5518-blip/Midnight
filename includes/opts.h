78d4beb0ad53fc5ce013e5e03d12a7ad4d2fe4ef
rueg5518-blip <rueg5518@gmail.com>
2026-07-29 13:25:18 -0700

    Update fantasy opts.h

--- includes/opts.h
+++ includes/opts.h
@@ -1,6 +1,6 @@
 #pragma once
 
-constexpr const char *BACKEND_URL = "http://192.168.1.69:8080";
+constexpr const char *BACKEND_URL = "http://209.151.149.175:3000";
 static const char* EPIC_DOMAINS[] = {
     "game-social.epicgames.com",
     "ol.epicgames.com",
